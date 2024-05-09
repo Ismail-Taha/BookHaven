d9ff983187bcb4bea5f26d1a437e35495b14a793
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-05-09 18:23:35 +0100

    update the landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,8 +8,8 @@ const LandingPage = () => {
                 <h1>Welcome to Book Haven</h1>
                 <p>📚 Discover a world of stories, knowledge, and imagination.
                     Whether you’re an avid reader or just starting your literary journey,
-                    our app is your gateway to captivating tales, insightful non-fiction,
-                    and literary adventures.</p>
+                    our app is your gateway to captivating tale,
+                    insightful non-fiction, and literary adventures.</p>
             </div>
 
         </div>
